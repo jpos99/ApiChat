230f2d7372d8cc8bcea3241b24fc0525ea7cf0ae
Joao Pedro Soares <jpos99@hotmail.com>
2023-07-27 15:37:51 -0300

    ajuste import

--- Chat.py
+++ Chat.py
@@ -1,24 +1,13 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException
-from fastapi.exceptions import RequestValidationError, ResponseValidationError
-from starlette.responses import JSONResponse
-
-import service
-from chatsac_API import ChatSacAPIService
+from fastapi import FastAPI, BackgroundTasks
+from service import *
 import uvicorn
 
 chatapp = FastAPI()
-chatsac = ChatSacAPIService()
-
-
-@chatapp.get('/')
-async def home():
-	channel_status = chatsac.get_channel_status()
-	return channel_status["status"]
 
 
 @chatapp.post('/dispatch')
 async def schedule_text_message(contacts: dict, background_tasks: BackgroundTasks) -> dict:
-	background_tasks.add_task(service.schedule_to_send_messages, contacts['contacts'], contacts['message'], contacts['date'])
+	background_tasks.add_task(schedule_to_send_messages, contacts['contacts'], contacts['message'], contacts['date'])
 	return {"status": "Recebido"}
 
 
